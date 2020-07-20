7734cf6d3ceeaef52453f59d574ff4adfc754399
Tim Griffith <grifmang@gmail.com>
2020-07-19 20:39:46 -0400

    edits

--- src/pages/AdminStoriesPage.jsx
+++ src/pages/AdminStoriesPage.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
 import { AdminNav, AdminStories } from "../components"
+import { useState } from 'react'
+import { useEffect } from 'react';
+import { AxiosWithAuth } from '../utils';
 
 export function AdminStoriesPage() {
+
+    const [stories, setStories] = useState([]);
+
+    useEffect(() => {
+        AxiosWithAuth()
+        .get('admin/')
+        .then(response => {
+            setStories(response.data.submissions);
+        })
+        .catch(err => console.log(err));
+    }, [])
+
     return (
         <>
             <AdminNav>
@@ -10,7 +25,7 @@ export function AdminStoriesPage() {
                     <h2 className="m-3">Top 10 Stories</h2>
                 </header>
             </AdminNav>
-            <AdminStories/>  
+            <AdminStories stories={stories} />  
         </>
     )
 }
