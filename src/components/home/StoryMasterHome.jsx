2f1016d3a29ef66e4cd91221726b730df917b627
Tim Griffith <grifmang@gmail.com>
2020-07-20 14:45:16 -0400

    small edits

--- src/components/home/StoryMasterHome.jsx
+++ src/components/home/StoryMasterHome.jsx
@@ -8,7 +8,7 @@ export function StoryMasterContent() {
             <h1 className="m-4 text-center ss-title text-primary">Story Squad</h1>
             <h3 className="m-4 text-center">Join us in our fun Daily Writing Contest</h3>
             {
-                about.map(each => <AboutContent subtitle={each.section.subtitle} paragraph={each.section.description} />)
+                about.map((each, inde) => <AboutContent key={inde} subtitle={each.section.subtitle} paragraph={each.section.description} />)
             }
         </div>
     )
