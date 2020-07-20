31213afcbc9a7ee2f678617084efbb3023687020
squashgray <squashgray@gmail.com>
2020-07-20 12:14:35 -0700

    sign-in, sign-up, navbar edits

--- src/pages/TopThreeWinnersPage.jsx
+++ src/pages/TopThreeWinnersPage.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { WinnerBoard } from '../components'
+import React from "react";
+import { WinnerBoard, Navbar } from "../components";
 
 export function TopThreeWinnersPage() {
-    return (
-        
-        <div className="custom-bg d-flex justify-content-center align-items-center">
-            <section className="topthreewinner text-center container-sm">
-                <h2 className="text-primary m-5">Top Three Winners</h2>
-                <WinnerBoard />
-            </section>
-        </div>
-    )
+  return (
+    <div className="custom-bg d-flex justify-content-center align-items-center">
+      <section className="topthreewinner text-center container-sm">
+        <Navbar />
+        <h2 className="text-primary m-5">Top Three Winners</h2>
+        <WinnerBoard />
+      </section>
+    </div>
+  );
 }
