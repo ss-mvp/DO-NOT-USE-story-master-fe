6051ba05b9b8149c2d74817e63b11066a75c8429
Alexander Sierra <alexandercsierra@gmail.com>
2020-08-18 18:10:21 -0400

    dashboard adapts to current time

--- src/pages/MobileDashboard.jsx
+++ src/pages/MobileDashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useHistory, useParams, useLocation } from 'react-router-dom';
+import { useHistory, useParams, useLocation, Link } from 'react-router-dom';
 import axios from 'axios';
 import Countdown from 'react-countdown';
 import moment from 'moment';
@@ -10,8 +10,22 @@ export function MobileDashboard() {
   const [end, setEnd] = useState();
   const [newGame, setNewGame] = useState();
   const [now, setNow] = useState(Date.parse(new Date()));
-  const [current, setCurrent] = useState('Submit your story');
+  const [current, setCurrent] = useState(0);
   const history = useHistory();
+  const [countdown, setCountdown] = useState(Date.now());
+  const [nextSlot, setNextSlot] = useState(false);
+
+  // const subEndVoteStart = moment(Number(end)).subtract({ hours: 3 });
+  // const voteStartTime = moment(Number(end)).subtract({ hours: 3 });
+  // const voteEndTime = moment(Number(end)).subtract({ minutes: 30 });
+  // const winnerTime = Number(end);
+
+  const subEndVoteStart = moment(Number(end)).subtract({
+    hours: 10,
+    minutes: 25,
+  });
+  const voteEndTime = moment(Number(end)).subtract({ hours: 10, minutes: 0 });
+  const winnerTime = Number(end);
 
   useEffect(() => {
     AxiosWithAuth()
@@ -20,28 +34,94 @@ export function MobileDashboard() {
         setTime(response.data.time.start.time);
         setEnd(response.data.time.end.end);
         setNewGame(response.data.time.newGame.newGame);
+        console.log('end from server', response.data.time.end.end);
       })
       .catch((err) => console.log(err));
   }, []);
 
-  const routes = {
-    'Submit your story': '/submission',
-    'Rank your favorites': '/ranking',
-    "Watch winners' livestream": '/announcement',
-    "See today's top 3": '/winners',
+  useEffect(() => {
+    if (time && end) {
+      incrementTimeSlot();
+    }
+  }, [time, end]);
+
+  const incrementTimeSlot = () => {
+    console.log('from increment time slot');
+    let currentTime = Date.now();
+    console.log(currentTime, Number(end));
+    //game is still going for today
+    if (currentTime < winnerTime) {
+      //game has already begun
+      if (currentTime >= time) {
+        //submission time
+        if (currentTime < subEndVoteStart) {
+          console.log('submission time');
+          setCurrent(0);
+          setCountdown(routes[0].end);
+        }
+        //voting time
+        else if (currentTime >= subEndVoteStart && currentTime < voteEndTime) {
+          console.log('voting time');
+          setCurrent(1);
+          setCountdown(routes[1].end);
+        } else if (currentTime >= voteEndTime && currentTime < winnerTime) {
+          console.log('livestream time');
+          setCurrent(2);
+          setCountdown(routes[2].start);
+        }
+      }
+    } else {
+      setCurrent(3);
+    }
   };
+
+  // incrementTimeSlot();
+  const routes = [
+    {
+      btnText: 'Submit your story',
+      route: '/submission',
+      subtitle: 'left to submit!',
+      start: Number(time),
+      end: subEndVoteStart,
+    },
+    {
+      btnText: 'Rank your favorites',
+      route: '/ranking',
+      subtitle: 'left to vote!',
+      start: subEndVoteStart,
+      end: voteEndTime,
+    },
+    {
+      btnText: "Watch winners' livestream",
+      route: '/announcement',
+      subtitle: '',
+      start: Number(end),
+    },
+    {
+      btnText: 'Check back tomorrow for the next prompt!',
+      route: '/mobiledash',
+      subtitle: '',
+    },
+  ];
 
   //navigate to different page depending on button text
   const navigate = () => {
-    history.push(routes[current]);
+    history.push(routes[current].route);
   };
 
-  const Completionist = () => <span>Game is over until 10:30PM.</span>;
+  const Completionist = () => {
+    incrementTimeSlot();
+    return <Countdown date={Number(subEndVoteStart)} renderer={renderer} />;
+  };
 
   const renderer = ({ hours, minutes, seconds, completed }) => {
     // console.log(completed)
     if (completed) {
-      return <Completionist />;
+      console.log('completed!');
+      // incrementTimeSlot();
+      window.location.reload();
+
+      return <p>reloading</p>;
     } else {
       return (
         <span>
@@ -52,21 +132,26 @@ export function MobileDashboard() {
       );
     }
   };
-
+  console.log(routes[current].btnText);
   return (
     <div className="custom-bg d-flex flex-column justify-content-center align-items-center">
       <h1 className="dashboard-title">
         What's Happening <span style={{ color: '#429CB5' }}>Now</span>?
       </h1>
       <button className="now" onClick={navigate}>
-        {current}
+        {routes[current].btnText}
       </button>
-      <div className="countDiv d-flex justify-content-center align-items-center flex-column">
-        {time && end && newGame ? (
-          <Countdown date={parseInt(end)} renderer={renderer} />
-        ) : null}
-        <p className="currentInstruction">left to submit!</p>
-      </div>
+      {current < 3 && (
+        <div className="countDiv d-flex justify-content-center align-items-center flex-column">
+          {time && end && newGame ? (
+            <Countdown date={Number(routes[current].end)} renderer={renderer} />
+          ) : null}
+          <p className="currentInstruction">{routes[current].subtitle}</p>
+        </div>
+      )}
+      <p style={{ marginTop: '4%' }}>
+        Check out the most recent winners <Link to="/winners">here.</Link>{' '}
+      </p>
     </div>
   );
 }
