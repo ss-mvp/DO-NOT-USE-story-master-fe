47bad2473a9948e8adf76be1d9298f18f7f93cab
Alexander Sierra <alexandercsierra@gmail.com>
2020-08-20 23:22:09 -0400

    fixed frontend modals and adjusted dashboard route string

--- src/pages/DashboardContainer.jsx
+++ src/pages/DashboardContainer.jsx
@@ -28,7 +28,7 @@ export default function DashboardContainer() {
     },
     {
       btnText: 'Check back tomorrow for the next prompt!',
-      route: '/mobiledash',
+      route: '/dashboard',
     },
   ];
 
