0f53ab3f35d06fc95add2eb9b9ad0b3df13ba8ed
Ben <bigriffin94@outlook.com>
2020-09-21 15:42:06 -0700

    sign up page corrected

--- src/components/signin/SignIn.jsx
+++ src/components/signin/SignIn.jsx
@@ -42,7 +42,7 @@ export function SignIn(props) {
           });
         } else {
           alert('You need to activate your email!');
-          history.push(`${baseUrl}/email/login`)
+
         }
       });
    
