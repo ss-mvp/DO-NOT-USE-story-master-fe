e089fdf3f37d4f24ce8ce6944be9c1f27dc607b7
Ben <bigriffin94@outlook.com>
2020-10-07 11:33:10 -0700

    Ranking Access Updated: users can only access the rank your favorites page when the winners length is exactly 3

--- src/components/nav/Menu.jsx
+++ src/components/nav/Menu.jsx
@@ -5,9 +5,9 @@ import ThreeWinnersNeededModal from "../progressBar/ThreeWinnersNeededModal";
 
 export default function Menu({loc}) {
 
-  const history = useHistory();
+  // const history = useHistory();
   const [winners, setWinners] = useState([]);
-  const [isDisabled, setIsDisabled] = useState(true)
+  
 
   useEffect(() => {
     
@@ -17,30 +17,10 @@ export default function Menu({loc}) {
         let response = res.data;
         console.log("RESPONSE", response)
         if(response.length > 0){
-          setIsDisabled(false)
           setWinners(response);
         }
       });
   }, []);
-
-
-      // Allow access to Rank Your Favorites only when winners === 3
-      const handleAllowAccess = () => {
-        // winners.length = 3;
-        // IF winners.length < 3 => history.push("/dashboard")
-        // keep users from going to ranking page
-        // present user with MODAL telling them that we are still waiting on 3 top submissions
-        if (winners.length < 3) {
-            console.log("WINNERS is < 3!", winners)
-            // history.push("/dashboard")
-        }
-        // IF winners.length is 3 
-        // send users to the ranking page
-        if (winners.length === 3) {
-            history.push("/ranking")
-            console.log("Winners is === 3", winners)
-        }
-    }
 
   if(!loc){
     return (
@@ -54,10 +34,10 @@ export default function Menu({loc}) {
         <Link to="/winners" className="nav-item nav-link">
           <h4 className="ss-title h4-nav">Top 3 Stories</h4>
         </Link>
-
-        {/* if winners === 3 allow access else Modal */}
-          <Link onClick={handleAllowAccess}
-          to="/ranking" className="nav-item nav-link">
+        {/* if winners.length === 3 allow access else Modal */}
+        <Link
+          to={winners.length === 3 ? "/ranking" : "/submission" }
+          className="nav-item nav-link">
           <h4 className="ss-title h4-nav">Rank your favorites</h4>
        </Link>
 
--- src/components/progressBar/ThreeWinnersNeededModal.jsx
+++ src/components/progressBar/ThreeWinnersNeededModal.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function ThreeWinnersNeededModal({closeModal}) {
 
     return (
-<div class="alert alert-warning alert-dismissible fade show" role="alert">
+<div className="alert alert-warning alert-dismissible fade show" role="alert">
   <strong>You are early!</strong> Check back around 3:30pm Eastern / 12:30 Pacific
 to see today's finalists
-  <button onClick={closeModal} type="button" class="close" data-dismiss="alert" aria-label="Close">
+  <button onClick={closeModal} type="button" className="close" data-dismiss="alert" aria-label="Close">
     <span aria-hidden="true">&times;</span>
   </button>
 </div>
