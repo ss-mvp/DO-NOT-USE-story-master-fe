815e278e88802cfbe514b7b69b9ccb842cd1b319
Logan Van Hook <loganvan@att.net>
2020-10-12 17:32:12 -0400

    New image get (#59)
    * Setup new image-getter
    
    * Grab images through BE
    
    * Add package-lock to ignore
    
    * RM Package-lock
    
    * Error catch

--- src/components/winners/modals/StoryModals.jsx
+++ src/components/winners/modals/StoryModals.jsx
@@ -1,6 +1,33 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { AxiosWithAuth } from '../../../utils';
 
 export default function StoryModal({ username, image, id }) {
+  let [SubData, sSubData] = useState();
+  useEffect(() => {
+    async function GetIt(_id)
+    {
+      AxiosWithAuth().get(`upload/image/${_id}`, { responseType: "arraybuffer" })
+        .then((response) =>
+        {
+          let image = btoa(
+            new Uint8Array(response.data)
+              .reduce((data, byte) => data + String.fromCharCode(byte), '')
+          );
+          sSubData(`data:${response.headers['content-type'].toLowerCase()};base64,${image}`);
+        }).catch((err) =>
+        {
+          console.log(err);
+          return;
+        });
+    }
+    GetIt(image);
+  }, []);
+
+  if (!SubData)
+    return(<>Loading...</>)
+  
   return (
     <>
       <button
@@ -24,7 +51,7 @@ export default function StoryModal({ username, image, id }) {
               <h4 className="p-3">Story</h4>
             </div>
             <div className="modal-body">
-              <img src={image} alt="submission" />
+              <img src={SubData} alt="submission" />
             </div>
           </div>
         </div>
