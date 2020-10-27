90376fd60954171f69d956a67593f21c253a60a9
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-10-27 15:28:03 -0700

    Added functionality to rotate submission image for better readability. (#73)

--- src/components/winners/modals/StoryModals.jsx
+++ src/components/winners/modals/StoryModals.jsx
@@ -1,28 +1,34 @@
-import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
-import { AxiosWithAuth } from "../../../utils";
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { AxiosWithAuth } from '../../../utils';
 
 export default function StoryModal({ username, image, id }) {
+  const [imgRotation, setImgRotation] = useState(0);
+
   let [SubData, sSubData] = useState();
+
+  const rotateRight = () => setImgRotation((cur) => (cur >= 3 ? 0 : cur + 1));
+  const rotateLeft = () => setImgRotation((cur) => (cur <= 0 ? 3 : cur - 1));
+
   useEffect(() => {
     async function GetIt(_id) {
       AxiosWithAuth()
-        .get(`upload/image/${_id}`, { responseType: "arraybuffer" })
-        .then(response => {
+        .get(`upload/image/${_id}`, { responseType: 'arraybuffer' })
+        .then((response) => {
           let image = btoa(
             new Uint8Array(response.data).reduce(
               (data, byte) => data + String.fromCharCode(byte),
-              ""
+              ''
             )
           );
           sSubData(
             `data:${response.headers[
-              "content-type"
+              'content-type'
             ].toLowerCase()};base64,${image}`
           );
         })
-        .catch(err => {
+        .catch((err) => {
           console.log(err);
           return;
         })
@@ -36,27 +42,62 @@ export default function StoryModal({ username, image, id }) {
   return (
     <>
       <button
-        className='btn btn-outline-primary m-2 px-4'
-        data-toggle='modal'
+        className="btn btn-outline-primary m-2 px-4"
+        data-toggle="modal"
         data-target={`#storyModal ${id}`}
       >
         {`${username}'s Story`}
       </button>
 
       <div
-        className='modal fade'
+        className="modal fade"
         id={`storyModal ${id}`}
-        tabIndex='-1'
-        role='dialog'
-        aria-hidden='true'
+        tabIndex="-1"
+        role="dialog"
+        aria-hidden="true"
       >
-        <div className='modal-dialog modal-dialog-centered'>
-          <div className='modal-content'>
-            <div className='title bg-primary text-white'>
-              <h4 className='p-3'>Story</h4>
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <div className="title bg-primary text-white">
+              <h4 className="p-3">Story</h4>
             </div>
-            <div className='modal-body'>
-              <img src={SubData} alt='submission' />
+            <div className="modal-body">
+              <img
+                src={SubData}
+                alt="submission"
+                style={{ transform: `rotate(${imgRotation * 90}deg)` }}
+              />
+            </div>
+            <div
+              className="modal-controls"
+              style={{
+                width: '100%',
+                display: 'flex',
+                justifyContent: 'space-between',
+              }}
+            >
+              <img
+                src={require('../../../assets/rotate-left.png')}
+                alt="rotate left"
+                style={{
+                  width: '50px',
+                  zIndex: '200',
+                  padding: '10px',
+                  cursor: 'pointer',
+                }}
+                onClick={rotateLeft}
+              />
+              <img
+                src={require('../../../assets/rotate-right.png')}
+                alt="rotate right"
+                style={{
+                  width: '50px',
+                  zIndex: '200',
+                  padding: '10px',
+                  cursor: 'pointer',
+                }}
+                onClick={rotateRight}
+              />
             </div>
           </div>
         </div>
