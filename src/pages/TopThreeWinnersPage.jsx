9e30cdabee25f500a10051ee042f04e4d6a3b91d
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-10-28 17:01:10 -0700

    Added reusable modal component as well as a histogram component on the results page. (#78)
    Co-authored-by: lorichl-otter <lori.schlatter@gmail.com>
    Co-authored-by: B-Griffinn <bigriffin94@outlook.com
    >

--- src/pages/TopThreeWinnersPage.jsx
+++ src/pages/TopThreeWinnersPage.jsx
@@ -1,15 +1,46 @@
-import React from "react";
-import { WinnerBoard, Navbar } from "../components";
-import { SEO } from "../utils";
+import React, { useEffect, useState } from 'react';
+import { WinnerBoard, Navbar } from '../components';
+import { AxiosWithAuth, SEO } from '../utils';
+import Modal from '../components/modal/Modal';
+import Histogram from '../components/histogram/Histogram';
 
 export function TopThreeWinnersPage(props) {
+  const [showModal, setShowModal] = useState(false);
+  const [histogram, setHistogram] = useState({});
+
+  useEffect(() => {
+    let mounted = true;
+    AxiosWithAuth()
+      .get('/ranking/histogram')
+      .then((res) => {
+        if (mounted) setHistogram(res.data);
+      })
+      .catch((err) => console.log(err));
+    return () => (mounted = false);
+  }, []);
+
   return (
     <div className="custom-bg d-flex justify-content-center align-items-center">
       <SEO title="Winners" path={props.match.path} />
       <section className="topthreewinner text-center container-sm">
+        <Modal
+          component={Histogram}
+          visible={showModal}
+          setVisible={setShowModal}
+          data={histogram.data}
+          layout={histogram.layout}
+        />
         <Navbar />
         <h2 className="text-center">Top Three Winners</h2>
         <WinnerBoard />
+        <p>
+          <button
+            style={{ marginTop: '10px' }}
+            onClick={() => setShowModal(true)}
+          >
+            View Your Score
+          </button>
+        </p>
       </section>
     </div>
   );
